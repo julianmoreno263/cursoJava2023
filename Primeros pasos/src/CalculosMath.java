658ee6a9b93086231d3965d67e677f911a6b3646
julianmoreno263 <julianmoreno263@gmail.com>
2023-03-02 13:53:18 -0500

    constantes con final, ejemplo clase Math

---
+++ Primeros pasos/src/CalculosMath.java
@@ -0,0 +1,8 @@
+public class CalculosMath {
+    public static void main(String[] args) {
+
+        double raiz = Math.sqrt(9);
+
+        System.out.println(raiz);
+    }
+}
--- Primeros pasos/src/DeclaracionesOperadores.java
+++ Primeros pasos/src/DeclaracionesOperadores.java
@@ -1,10 +1,14 @@
 public class DeclaracionesOperadores {
 
     public static void main(String[] args) {
-        int a = 5;
-        int b = 7;
-        int c = b + a;
 
-        System.out.println(c);
+        int c = 5;// variable
+
+        final double apulgadas = 2.54;// constante se declara con final
+        double cm = 6;
+
+        double resultado = cm / apulgadas;
+
+        System.out.println("En " + cm + "cm hay " + resultado + "pulgadas");
     }
 }
