8d78c42a33a2698e03a7e0a28e47aad77231e27f
julianmoreno263 <julianmoreno263@gmail.com>
2023-03-03 13:50:58 -0500

    Trabajo con clase Math y clase String

--- Primeros pasos/src/CalculosMath.java
+++ Primeros pasos/src/CalculosMath.java
@@ -1,8 +1,26 @@
 public class CalculosMath {
     public static void main(String[] args) {
 
-        double raiz = Math.sqrt(9);
+        // para ver que reciben por parametro los metodos,debemos tener a la mano la api
+        // de java y asi usar bien los metodos de las clases,aqui el metodo sqrt
+        // devuelve un double,por eso la variable donde almacenemos el resultado debemos
+        // declararla como double
+        // double raiz = Math.sqrt(9); //sqrt para sacar raiz cuadrada
 
-        System.out.println(raiz);
+        double num1 = 5.8;
+        double base = 5;
+        double exponente = 3;
+
+        // el metodo round retorna un int, si le pasamos un double retorna un dato de
+        // tipo long,osea me saca un error porque intentamos guardar un long en un int,
+        // aqui usamos refundicion o casteo para pasar el tipo de dato a otro,lo
+        // indicamos al inicio de la instruccion entre parentesis.
+        // int resultado = (int) Math.round(num1);//round redondea numeros
+
+        // metodo pow es para calcular la potencia de un numero,pide la base y exponente
+        // ambos de tipo double,retorna un double
+        double resultado = Math.pow(base, exponente);
+
+        System.out.println("El resultado de la operación es " + resultado);
     }
 }
