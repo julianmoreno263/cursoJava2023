f3756da9b3fd99a525c2dfcfcc17cc973a1181c1
julianmoreno263 <julianmoreno263@gmail.com>
2023-03-08 13:17:02 -0500

    comenzando con poo,ordenando codigo en paquetes

---
+++ Primeros pasos/src/poo/UsoCoche.java
@@ -0,0 +1,13 @@
+
+/*aqui vamos a crear objetos de la clase Coche,para eso utilizamos el nombre de la clase Coche y despues el  constructor de la clase Coche por medio de "new Coche()", ya despues de creado el objeto puedo comenzar a ver sus propiedades y metodos. */
+
+package poo;
+
+public class UsoCoche {
+    public static void main(String[] args) {
+
+        Coche Renault = new Coche();// instanciamos la clase Coche
+
+        System.out.println("El coche Renault tiene " + Renault.ruedas + " ruedas");
+    }
+}
