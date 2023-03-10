c321453eb89cd771c4231d0c46e3e782eb696717
Julián Moreno <julianmoreno263@gmail.com>
2023-03-10 17:33:46 -0500

    ejercicio con getters y setters

--- Primeros pasos/src/poo/Coche.java
+++ Primeros pasos/src/poo/Coche.java
@@ -20,12 +20,21 @@
  3-nombre del metodo y entre {} el codigo del metodo.
 
  NOTA: AQUI EN VSC CON EL PLUGIN QUE INSTALE DE JAVA, PONGO GET O SET Y EL ME DA LAS OPCIONES PARA CREAR AUTOMATICAMENTE LOS METODOS GETTER Y SETTER SEGUN LAS PROPIEDADES QUE TENGA DEFINIDAS.
+
+ A los metodos se les pueden pasar parametros, estos son valores que se les pueden pasar para que operen con esos valores y asi el usuario que llame al metodo le pueda pasar un valor que quiera para que el metodo ejecute el codigo con ese valor,en el momento el setter setColor establece el color en azul,es fijo,pero si quiero que los objetos que yo haga sean de colores diferentes les paso un parametro. Cuando vayamos a llamar dentro del metodo a una variable o a otro metodo,debemos referenciar estas variables o metodos con "this", This hace referencia al objeto actual de la clase, es decir, a un objeto concreto de la clase y nos sirve para usar los métodos y atributos de esa clase desde alguno de sus métodos, para llamar a otro de sus constructores o simplemente para pasarle el objeto completo a algún otro método u objeto.Cuando a un metodo le pasamos un parametro con el mismo nombre de una propiedad de la clase,por ejemplo al setter de setColor() le pasamos el parametro color setColor(color) podemos diferenciar la variable color de la clase con el parametro color con el this, el this.color hace referencia a la variable de la clase,y asi no hay confusion.
+
+ NOTA: RECORDAR QUE TODAS LAS PROPIEDADES DEBEN SER PRIVATE,DEBEN IR ENCAPSULADAS PARA QUE NO SE PUEDAN MANIPULAR DESDE OTRA CLASE A MENOS QUE SE USEN LOS METODOS INDICADOS,LOS CUALES SON LOS QUE PERMITEN LA COMUNICACION ENTRE CLASES.
+
+ NOTA: EN LOS IF DE LOS SETTERS DONDE COMPARAMOS EL PARAMETRO DE TIPO sTRING CON EL STRING "SI", LO HACEMOS USANDO EL METODO EQUALS,PORQUE EN JAVA SE DEBE USAR EL EQUALS PARA COMPARAR STRINGS,NO SE USA EL ==, AUNQUE EN ESTE CODIGO FUNCIONARIA IGUAL,PERO SE DEBE USAR EL METODO EQUALS DE LA CLASE STRING.
+
+
  */
 
 package poo;
 
 public class Coche {
 
+    // declaramos propiedades
     private int ruedas;
     private int largo;
     private int ancho;
@@ -33,9 +42,9 @@ public class Coche {
     private int pesoChasis;
 
     // estas propiedades podran variar
-    String color;
-    int pesoTotal;
-    boolean asientosCuero, climatizador;
+    private String color;
+    private int pesoTotal;
+    private boolean asientosCuero, climatizador;
 
     // metodo constructor
     public Coche() {
@@ -47,18 +56,106 @@ public class Coche {
         pesoChasis = 500;
     }
 
+    // --------------------------------------------------------------------------------------------
+
     // metodos getters
-    public String getLargo() {
-        return "El largo del coche es " + largo + " cms";
+    // public String getLargo() {
+    // return "El largo del coche es " + largo + " cms";
+    // }
+
+    public String getDatosGenerales() {
+        return "El vehículo tiene " + ruedas + " ruedas, mide " + (largo / 1000) + " mts, con un ancho de "
+                + (ancho) + " cms, y un peso de chasis de " + pesoChasis + " kg.";
     }
 
     public String getColor() {
         return "El color del coche es " + color;
     }
 
+    public String getAsientos() {
+
+        if (this.asientosCuero == true) {
+
+            return "El coche tiene asientos de cuero";
+        } else {
+
+            return "El coche  tiene asientos de fabrica";
+        }
+    }
+
+    public String getClimatizador() {
+
+        if (this.climatizador == true) {
+
+            return "El coche tiene climatizador";
+        } else {
+
+            return "El coche  tiene aire acondicionado";
+        }
+    }
+
+    // -------------------------------------------------------------------------------
+
     // setters
-    public void setColor() {
-        color = "azul";
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setAsientosCuero(String asientosCuero) {
+
+        if (asientosCuero.equals("si")) {
+            this.asientosCuero = true;
+        } else {
+            this.asientosCuero = false;
+        }
+    }
+
+    public void setClimatizador(String climatizador) {
+
+        if (climatizador.equals("si")) {
+            this.climatizador = true;
+        } else {
+            this.climatizador = false;
+        }
+
+    }
+
+    // aqui vamos a hacer un metodo que sera un setter y un getter a la vez,esto no
+    // se debe hacer por buenas practicas,lo correcto es hacer los setters y getters
+    // separados,pero para este ejercicio no hay mucho problema, es un getter porque
+    // nos devuelve en un return el pesoTotal, osea nos devuelve algo, y es un
+    // setter porque aqui estamos estableciendo valores para las variables.
+    public String getPesoTotal() {
+
+        int pesoCarroceria = 500;
+        this.pesoTotal = this.pesoChasis + pesoCarroceria;
+
+        if (this.asientosCuero == true) {
+            pesoTotal = pesoTotal + 50;
+        }
+        if (this.climatizador == true) {
+            pesoTotal = pesoTotal + 20;
+        }
+
+        return "El peso total del coche es " + pesoTotal;
+    }
+
+    // aqui hacemos otro metodo getter normal, porque no estamos cambiando el valor
+    // de ninguna variable de clase, ,inicialmente los vehiculos sin accesorios
+    // valdran 10000,si llevan accesorios van aumentando el valor.Utilizamos el
+    // operador += que es lo mismo que hacer precioFinal=precioFinal+2000.
+
+    public int getPrecioFinal() {
+        int precioFinal = 10000;
+
+        if (this.asientosCuero == true) {
+            precioFinal += 2000;
+        }
+        if (this.climatizador == true) {
+            precioFinal += 1500;
+        }
+
+        return precioFinal;
     }
 
 }
--- Primeros pasos/src/poo/UsoCoche.java
+++ Primeros pasos/src/poo/UsoCoche.java
@@ -1,18 +1,39 @@
 
-/*aqui vamos a crear objetos de la clase Coche,para eso utilizamos el nombre de la clase Coche y despues el  constructor de la clase Coche por medio de "new Coche()", ya despues de creado el objeto puedo comenzar a ver sus propiedades y metodos. Recordar que para que un objeto pueda acceder a las propiedades de la clase donde esta el constructor debe hacerlo por medio de los metodos,asi se comunican las clases.Como creamos el objeto Mazda y queremos darle un color,debemos hacerlo por medio del metodo setColor(), y despues para verlo en consola debemos hacerlo por medio del metodo getColor().*/
+/*aqui vamos a crear objetos de la clase Coche,para eso utilizamos el nombre de la clase Coche y despues el  constructor de la clase Coche por medio de "new Coche()", ya despues de creado el objeto puedo comenzar a ver sus propiedades y metodos. Recordar que para que un objeto pueda acceder a las propiedades de la clase donde esta el constructor debe hacerlo por medio de los metodos,asi se comunican las clases.Como creamos el objeto Mazda y queremos darle un color,debemos hacerlo por medio del metodo setColor(), y despues para verlo en consola debemos hacerlo por medio del metodo getColor(). Podemos utilizar la clase JOptionPane de javax.swing para que el programa le pregunte al usuario los datos y este los ingrese por las ventanas que salen.*/
 
 package poo;
+
+import javax.swing.*;
 
 public class UsoCoche {
     public static void main(String[] args) {
 
         Coche Renault = new Coche();// instanciamos la clase Coche
-        Coche Mazda = new Coche();
-        Mazda.setColor();
+        Renault.setColor(JOptionPane.showInputDialog("Establece un color", Renault));
+        Renault.setAsientosCuero(JOptionPane.showInputDialog("Tiene asientos de cuero? si/no", Renault));
+        Renault.setClimatizador(JOptionPane.showInputDialog("Tiene climatizador? si/no", Renault));
+
+        Coche Mazda = new Coche();// creamos otro objeto y usamos el setter
+        Mazda.setColor(JOptionPane.showInputDialog("Establece un color", Mazda));
+        Mazda.setAsientosCuero(JOptionPane.showInputDialog("Tiene asientos de cuero? si/no", Mazda));
+        Mazda.setClimatizador(JOptionPane.showInputDialog("Tiene climatizador? si/no", Mazda));
 
         // usamos los metodos getter para poder acceder a las propiedades
-        System.out.println(Renault.getLargo());
+        System.out.println(Renault.getDatosGenerales());
+        System.out.println(Renault.getAsientos());
+        System.out.println(Renault.getClimatizador());
+        System.out.println(Renault.getColor());
+        System.out.println(Renault.getPesoTotal());// aqui llamamos al metodo tipo getter-setter
+        System.out.println("El precio final del Renault es " + Renault.getPrecioFinal());
+
+        System.out.println("---------------------------------------------------");
+
+        System.out.println(Mazda.getDatosGenerales());
+        System.out.println(Mazda.getAsientos());
+        System.out.println(Mazda.getClimatizador());
         System.out.println(Mazda.getColor());
+        System.out.println(Mazda.getPesoTotal());// tipo getter-setter
+        System.out.println("El precio final del Mazda es " + Mazda.getPrecioFinal());
 
         // System.out.println("El coche Renault tiene " + Renault.ruedas + " ruedas");
 
