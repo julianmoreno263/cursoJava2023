8f8b9bddba54de02aa85a525413dc44a4fdb06c4
julianmoreno263 <julianmoreno263@gmail.com>
2023-05-01 16:06:51 -0500

    finalizacion ejercicio calculadora para grid layout y comienzo de tema swing clases JTextField JTextArea

--- Primeros pasos/src/graficos/Layout2.java
+++ Primeros pasos/src/graficos/Layout2.java
@@ -17,6 +17,13 @@ Para poner a la escucha del evento los botones lo hacemos dentro de la misma fun
 
 NOTA: PARA EDITAR VARIAS LINEAS AL TIEMPO EN VSC ES CON: ALT+BOTON IZQUIERDO DEL MOUSE.
 
+--------------------------------------------------------------------------
+(v85) vamos a darle funcionalidad a la calculadora,primero vamos a hacer que cuando queramos hacer una operacion con numeros, el numero que este en el display se quite y se ponga el nuevo numero,osea,cuando vayamos por ejemplo a hacer una suma,2+34,ponemos el 2,y al pulsar el + y escribir el 34 se quite el 2 y nos muestre el 34,asi funcionan las calculadoras.Para esto debemos poner a la escucha los botones de las operaciones,asi que debemos hacer otra clase interna para estos botones.Esta clase interna con la interfaz ActionListener lo unico que tendra sera una linea de codigo que pone la variable principio en true,esto es para que al pulsar un boton de alguna operacion se resetee el display.
+
+Ahora, creamos una variable para ir guardando el resultado de las operaciones,tambien creamos un metodo que en si sera el cerebro del programa,este metodo realizara el calculo de las operaciones,para esto le debo ir pasando por parametro lo que haya en el display,pero como el display muestra es texto y debo calcular con numeros,lo parseo con el metodo parseDouble de la clase Double.
+
+Tambien creamos una variable para ir poniendo la ultima operacion ejecutada por el usuario,porque si el usuario oprime el = debemos mostrar el resultado total.
+
 
  */
 
@@ -57,6 +64,8 @@ class LaminaDisplay extends JPanel {
     private JPanel miLamina2;
     private JButton pantalla;
     private boolean principio;
+    private double resultado;
+    private String ultimaOperacion = "=";
 
     public LaminaDisplay() {
 
@@ -67,8 +76,13 @@ class LaminaDisplay extends JPanel {
         pantalla.setEnabled(false);
         add(pantalla, BorderLayout.NORTH);
 
-        // creamos el objeto de tipo ActionListener para el oyente
+        // creamos el objeto de tipo ActionListener para el oyente de los botones
+        // numericos
         java.awt.event.ActionListener insertar = new InsertaNumero();
+
+        // creamos el objeto de tipo ActionListener para el oyente de los botones de las
+        // operaciones
+        java.awt.event.ActionListener orden = new AccionOrden();
 
         // segunda lamina para los botones,con el metodo ponerBoton creamos cada
         // boton,tener en cuenta que los botones se van agregando por filas.
@@ -77,19 +91,19 @@ class LaminaDisplay extends JPanel {
         ponerBoton("7", insertar);
         ponerBoton("8", insertar);
         ponerBoton("9", insertar);
-        // ponerBoton("/");
+        ponerBoton("/", orden);
         ponerBoton("4", insertar);
         ponerBoton("5", insertar);
         ponerBoton("6", insertar);
-        // ponerBoton("*");
+        ponerBoton("*", orden);
         ponerBoton("1", insertar);
         ponerBoton("2", insertar);
         ponerBoton("3", insertar);
-        // ponerBoton("-");
+        ponerBoton("-", orden);
         ponerBoton("0", insertar);
         ponerBoton(".", insertar);
-        // ponerBoton("=");
-        // ponerBoton("+");
+        ponerBoton("=", orden);
+        ponerBoton("+", orden);
 
         // agregamos esta lamina ala parte central de la primera
         add(miLamina2, BorderLayout.CENTER);
@@ -105,7 +119,7 @@ class LaminaDisplay extends JPanel {
         miLamina2.add(boton);
     }
 
-    // clase interna que maneja los eventos
+    // clase interna que maneja los eventos para los botones numericos
     private class InsertaNumero implements java.awt.event.ActionListener {
 
         public void actionPerformed(ActionEvent e) {
@@ -136,4 +150,50 @@ class LaminaDisplay extends JPanel {
 
         }
     }
+
+    // clase interna que maneja los eventos para los botones de las operaciones
+    private class AccionOrden implements java.awt.event.ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            // variable para guardar la operacion que el usuario pulso
+            String operacion = e.getActionCommand();
+
+            calcular(Double.parseDouble(pantalla.getText()));
+
+            ultimaOperacion = operacion;
+
+            principio = true;
+        }
+
+        public void calcular(double x) {
+
+            if (ultimaOperacion.equals("+")) {
+
+                resultado += x;
+
+            } else if (ultimaOperacion.equals("-")) {
+
+                resultado -= x;
+            } else if (ultimaOperacion.equals("*")) {
+
+                resultado *= x;
+            } else if (ultimaOperacion.equals("/")) {
+
+                resultado /= x;
+            } else if (ultimaOperacion.equals("=")) {
+
+                resultado = x;
+            }
+
+            // le ponemos el string "" para concatenarlo con la variable resultado y que no
+            // me de error,porque el metodo setText espera un string y si le ponemos
+            // resultado solo(el cual es un double) saca error,por eso lo concatenamos con
+            // un string.
+            pantalla.setText("" + resultado);
+        }
+
+    }
+
 }
