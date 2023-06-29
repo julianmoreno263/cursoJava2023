a64d2f81dc141c333d0fef0a70831364a2b47bd4
julianmoreno263 <julianmoreno263@gmail.com>
2023-06-29 17:43:00 -0500

    sincronizando multiples hilos en nuestra app del Banco implementando la interfaz Lock de la clase ReentrantLock y creando bloqueos con await(),e implementacion de condiciones en estos bloqueos con la interfaz Condition y el metodo newCondition() y signalAll()

--- ProgramacionConcurrente/src/usoThreads/BancoSinSincronizar.java
+++ ProgramacionConcurrente/src/usoThreads/BancoSinSincronizar.java
@@ -33,11 +33,64 @@
 
 8- listo,al ejecutar el programa vemos que el saldo total que deberia mantenerse en 200.000, en un momento es menor, a veces vuelve a 200.000 y despues vuelve y es menor. Recordar que hasta este punto el programa maneja hilos normales,no estan sincronizados.
 
+-----------------------------------------------------------------------
+(v175) vamos aver porque el saldo total se minimiza a veces, logico que esto es porque la aplicacion no tiene los hilos sincronizados pero vamos a ver en si que pasa,y ademas vamos a usar la clase ReentrantLock que sirve precisamente para sincronizar una aplicacion compleja que tiene multiples hilos, con join() sincronizamos hilos pero este sirve para aplicaciones mas basicas.
 
+En si esto del saldo total pasa porque e stamos trabajando con programacion concurrente,osea varios hilos ejecutandose al tiempo, el metodo transferiri es el que hace el trabajo del programa cuando es llamado por el metodo run(), entonces en el metodo transferir,por ejemplo un primer hilo comienza a transferir dindero de a hacia b, por ejemplo 200pesos, y puede darse,porque como estamos trabajando con random() que otro segundo hilo tambien comienze a transferir dinero entre estas mismas cuentas solo que va un poco mas atras que el primer hilo. Cuando el hilo1 llega al final e imprime el saldo total, puede ser que la cuenta no sea correcta porque antes de que el hilo1 acabe el hilo2 tambien desconto mas dinero de esa misma cuenta origen,asi cuando el hilo1 llega al final para imprimir el saldo total la resta es diferente a lo que iba con el hilo1 y por eso el saldo total llega en ciertos momentos con un monto menor al esperado, y esto puede darse porque los metodos random() al ser aleatorios en un momento pueden llegar a tomar los mismos valores para varios hilos que se van ejecutando al tiempo.
+
+Para solucionar esto en este tipo de programas es que se usa la clase ReentrantLock la cual implementa la interfaz Lock, y esta interfaz tiene varios metodos entre ellos el metodo lock y unlock.
+
+El metodo lock bloquea un trozo de codigo del programa para que solo pueda ser ejecutado por un hilo simultaneamente. Osea, por ejemplo puede bloquear 50 lineas de codigo,para que esas lineas solo puedan ser ejecutadas por un unico hilo, y unlock() desbloquea esas 50 lineas de codigo para que el siguiente hilo pueda ejecutarlas.
+
+Entonces, en nuestro programa, necesitamos bloquear con lock() el metodo transferencia() para que solo un hilo lo pueda acceder y asi ejecute su codigo, y luego al finalizar la ejecucion de ese hilo lo desbloqueamos con unlock() para que el hilo que viene atras ya pueda acceder y asi sucesivamente, por lo tanto no podran acceder varios hilos al tiempo en el metodo,solo sera uno a la vez y asi la cuenta del saldo total no se vera afectada.
+
+1- debemos usar la clase ReentrantLock por lo que debemos importar el paquete java.util.concurrent.locks.ReentrantLock;
+
+2- creamos una instancia de ReentrantLock la cual implemneta la interfaz Lock, en la clase Banco que es donde la vamos a usar porque alli esta el metodo transferencia que necesitamos bloquear.
+
+3- con esta instancia creada vamos al metodo transferencia y la utilizamos rodeando el codigo interno del metodo con un try-finally, en el try ira el lock() y en el finally el unlock() y asi queda bloqueado el codigo que necesitamos que se ejecute un hilo a la vez.
+
+Listo!, si ejecutamos el programa veremos que el saldo se mantiene en 200.000 y no se pierde en el camino por el bloqueo que hemos implementado.
+
+-----------------------------------------------------------------
+(v176) vamos a ver como crear condiciones para nuestros bloqueos,a veces sera necesario poner condiciones en los bloqueos. Para esto debemos conocer bien como trabaja nuestra app y sobre todo el metodo transferencia que es el que hace todo.
+
+1- digamos que la cuenta 25 se ha ejecutado varias veces a lo largo de la ejecucion del programa,osea ha realizado varias transferencias de plata,por lo que su saldo de 2000 iniciales habra bajado, si por ejemplo nuevamente se llega a esa cuenta y se quiere transferir dinero pero su saldo es menor al monto que se quiere transferir es alli donde entra el if que evalua esta condicion y si es cierto el return no permite que se realice esta accion y lo que hace el return es devolver el programa hacia la clase EjecucionTransferencias pues desde alli es que se hace la llmada al metodo transferencia. Esto quiere decir que el resto del codigo del metodo transferencia no se ejecutaria y ese hilo se pierde. Lo que si se ejecutaria siempre es lo que esta en el finally del try, que es la instruccion de desbloquear el codigo para que el otro hilo que este detras esperando pueda acceder a el.
+
+2- ahora, digamos que una condicion del negocio es que todas las transferencias deben de ejecutarse,osea no se puede perder ningun hilo, pero no pueden haber saldos negativos en las cuentas(porque si quitamos el return del if ya no se perdera el hilo,se ejecutaria todo, pero podrian darse cuentas con saldos negativos). Para esta situacion es que se usan condiciones en el bloqueo, y para esto se usa el metodo await(). Este metodo debe hacer dos cosas,pone el hilo que no se puede ejecutar porque no hay saldo suficiente a la espera,osea el hilo no se pierde sino que queda a la espera,y ademas libera el bloqueo para que continue otro hilo.
+
+3- ahora, si un hilo esta a la espera,por ejemplo hilo7, y llega el hilo8 y realiza un ingreso de dinero a una cuenta, el hilo8 debera hacer dos cosas cuando haga el ingreso de dinero,primero desbloquea el codigo y segundo debe avisar a los hilos en espera que ha echo un ingreso de dinero a una cuenta para que esos hilos miren a ver si ese ingreso les sirve para poder ejecutar la transferencia que esta a la espera, de esta forma todas las transferencias se realizaran,ninguna se pierde.
+
+4- entonces, para establecer en un bloqueo de hilos una condicion debemos usar el metodo newCondition() de la clasae ReentrantLock, este metodo devuelve un objeto de tipo Condition,esta es una interfaz y esta interfaz entre sus metodos tiene el metodo await(),este metodo pone a la espera a los hilos y este metodo lanza una excepcion de tipo InterruptedException.Tambien tiene el metodo signalAll() el cual le dice a los hilos que estan a la espera que despierten(ver API).
+
+---------------------------------------------------------
+(v177) vamos a ver como se crea la condicion en el codigo con bloqueo.
+
+1- creamos en la clase donde esta el codigo bloqueado,el metodo transferencia, una variable de tipo Condition.
+
+2- ahora, en el constructor de la clase Banco, indicamos que el bloqueo debera tener una condicion asi:
+
+                        saldoSuficiente=cierreBanco.newCondition()
+
+ponemos este codigo en el constructor para que cada vez que instanciemos un Banco, venga con un bloqueo con condicion.
+
+3- ahora, en el metodo transferencia, vez de poner un if en el try,lo cambiamos por un while dejando la misma condicion, y quitamos el return porque ahora necesitamos que todos los hilos(las transferencias) se ejecuten(con el return se pierden los hilos), y dentro de e ste while es que ponemos este codigo:
+
+                    saldoSuficiente.await()
+
+osea,le estamos diciendo al codigo que "mientras la condicion del while sea verdadera(osea que el saldo de la cuenta origen es menor a la cantidad a transferir) pues que ese hilo permanezca a la espera". Recordar que como await lanza una exception toca rodearlo con un try-catch,o podemos indicarle al metodo transferencia que saldra una exception de tipo InterruptedExcetion por medio de un throws,si hacemos esto debemos rodear la llamad al metodo transferencia con un try-catch el cual ya lo tenemos rodeado cuando hacemos esa llamada en el metodo run.
+
+4- por ultimo, con signalAll() le indicamos a los hilos que estan a la espera que despierten y miren si la condicion del while cambio para que puedan ejecutarse.Esto lo ponemos al final del try antes del finnaly,porque es en ese punto donde un hilo que si se pudo ejecutar finaliza su ejecucion,entonces apenas finaliza su ejecucion avisa a los que e stan a la espera que despierten.
+
+Listo, de esta forma con condiciones de bloqueo ningun hilo muere,solo quedan ala espera para poderse ejecutar despues y asi todas las transferencias se realizan.
 
  */
 
 package usoThreads;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class BancoSinSincronizar {
     public static void main(String[] args) {
@@ -61,6 +114,8 @@ public class BancoSinSincronizar {
 class Banco {
 
     private final double cuentas[];
+    private Lock cierreBanco = new ReentrantLock();
+    private Condition saldoSuficiente;
 
     // constructor
     public Banco() {
@@ -72,32 +127,55 @@ class Banco {
 
             cuentas[i] = 2000;
         }
+
+        // aqui establecemos que cada vez que instanciemos un Banco este venga con una
+        // condicion en el bloqueo.
+        saldoSuficiente = cierreBanco.newCondition();
     }
 
     // metodo para las transferencias entre cuentas
-    public void transferencia(int cuentaOrigen, int cuentaDestino, double cantidad) {
+    public void transferencia(int cuentaOrigen, int cuentaDestino, double cantidad) throws InterruptedException {
 
-        // aqui evaluamos que el saldo de la cuenta no sea inferior a la cantidad que
-        // queremos transferir,si esto pasa no deja hacer nada con el return.
-        if (cuentas[cuentaOrigen] < cantidad) {
-            return;
+        // aqui bloqueamos el codigo para que solo lo acceda un hilo a la vez
+        cierreBanco.lock();
+
+        try {
+            // aqui evaluamos que el saldo de la cuenta no sea inferior a la cantidad que
+            // queremos transferir,si esto pasa no deja hacer nada con el return.Pero al
+            // poner condicion de bloqueo usamos el metodo await() y deja a la espera el
+            // hilo.
+            while (cuentas[cuentaOrigen] < cantidad) {
+
+                saldoSuficiente.await();
+
+            }
+
+            // imprimimos el hilo que realiza la transferencia actual
+            System.out.println(Thread.currentThread());
+
+            // aqui descontamos de la cuenta origen la cantidad que queremos transferir
+            cuentas[cuentaOrigen] -= cantidad;
+
+            // mensaje que indica la cantidad a transferir y entre que cuentas,usamos printf
+            // para darle este formato y asi usar dos decimales.
+            System.out.printf("%10.2f de %d para %d", cantidad, cuentaOrigen, cuentaDestino);
+
+            // aqui incrementamos el saldo de la cuenta destino
+            cuentas[cuentaDestino] += cantidad;
+
+            // aqui imprio el saldo total
+            System.out.printf("Saldo total: %10.2f%n ", getSaldoTotal());
+
+            // con esta instruccion le indicamos a los hilos que estan ala espera que
+            // despierten y miren si la condicion del while a cambiado para que puedan
+            // ejecutarse
+            saldoSuficiente.signalAll();
+
+        } finally {
+
+            cierreBanco.unlock();
         }
 
-        // imprimimos el hilo que realiza la transferencia actual
-        System.out.println(Thread.currentThread());
-
-        // aqui descontamos de la cuenta origen la cantidad que queremos transferir
-        cuentas[cuentaOrigen] -= cantidad;
-
-        // mensaje que indica la cantidad a transferir y entre que cuentas,usamos printf
-        // para darle este formato y asi usar dos decimales.
-        System.out.printf("%10.2f de %d para %d", cantidad, cuentaOrigen, cuentaDestino);
-
-        // aqui incrementamos el saldo de la cuenta destino
-        cuentas[cuentaDestino] += cantidad;
-
-        // aqui imprio el saldo total
-        System.out.printf("Saldo total: %10.2f%n ", getSaldoTotal());
     }
 
     // metodo para ir calculando el saldo total, aqui el bucle va sumando los 2000
